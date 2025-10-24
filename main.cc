1e2e3f72619170bf9557786c9d56007076bec45d
fayed_1077611 <adamfayed918@gmail.com>
2025-10-24 13:20:24 -0700

    Thing 2 is success

--- main.cc
+++ main.cc
@@ -62,8 +62,8 @@ int function1() {
 int function2() {
 	int seed = read("What seed should we use for the random number generator?\n");
 	int sum{};
+	srand(seed);
 	for (int i = 0; i < 20; i++) {
-		srand(seed);
 		sum += rand() % 6 + 1;
 	}
 	return sum;
